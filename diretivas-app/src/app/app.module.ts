03debebe24fe65196e7af0d61064bcf27491599d
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2019-04-19 12:38:19 -0300

    Exemplo da diretive ngStyle

---
+++ diretivas-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+
+import { AppComponent } from './app.component';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatCardModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
