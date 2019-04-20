4528861498ff835f8ffade8be9bfa5ca19267c45
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2019-04-20 12:47:00 -0300

    Exemplo de ViewChild

--- communication-app/src/app/parent-child/parent-child.component.ts
+++ communication-app/src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  // Podemos passar a variavel de template ou o nome do componente
+  @ViewChild('stopWatch2')
+  private myTimer: TimerComponent;
+
+  @ViewChild('myP')
+  private myP: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  start() {
+    this.myTimer.start();
+  }
+  stop() {
+    this.myTimer.stop();
+  }
+  clear() {
+    this.myTimer.clear();
+  }
+
+  // Depois de tudo carregar vai ser iniciado
+  ngAfterViewInit() {
+    console.log(this.myP);
+    console.log(this.myTimer);
+  }
 }
