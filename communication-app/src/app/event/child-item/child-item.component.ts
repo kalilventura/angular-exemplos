a1717fb02f253eeef9c08ab22608b5f07744f3ce
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2019-04-19 22:03:21 -0300

    Event Emitter com parâmetros

--- communication-app/src/app/event/child-item/child-item.component.ts
+++ communication-app/src/app/event/child-item/child-item.component.ts
@@ -8,28 +8,12 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class ChildItemComponent implements OnInit {
 
   @Input() title: string;
-  @Output() plusOne = new EventEmitter<any>();
-  @Output() plusTwo = new EventEmitter<any>();
-  @Output() minusOne = new EventEmitter<any>();
-  @Output() minusTwo = new EventEmitter<any>();
+  @Output() inc = new EventEmitter<number>();
   constructor() {}
 
   ngOnInit() {}
 
-  plusOneClick() {
-    this.plusOne.emit();
-    console.log('+1');
-  }
-  plusTwoClick() {
-    this.plusTwo.emit();
-    console.log('+2');
-  }
-  minusOneClick() {
-    this.minusOne.emit();
-    console.log('-1');
-  }
-  minusTwoClick() {
-    this.minusTwo.emit();
-    console.log('-2');
+  btnClick(n) {
+    this.inc.emit(n);
   }
 }
