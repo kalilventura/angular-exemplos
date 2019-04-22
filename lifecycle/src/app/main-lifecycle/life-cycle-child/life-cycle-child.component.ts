d3a1b1f90a880ea8305cc02c0687b87e210b9cb9
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2019-04-21 23:33:51 -0300

    Explorando a fundo os ciclos de vida

--- lifecycle/src/app/main-lifecycle/life-cycle-child/life-cycle-child.component.ts
+++ lifecycle/src/app/main-lifecycle/life-cycle-child/life-cycle-child.component.ts
@@ -1,18 +1,82 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges, AfterContentInit, AfterViewInit } from '@angular/core';
+
+export interface LifeCycleEvent {
+  id: number;
+  name: string;
+  color: string;
+}
 
 @Component({
   selector: 'app-life-cycle-child',
   templateUrl: './life-cycle-child.component.html',
   styleUrls: ['./life-cycle-child.component.css']
 })
-export class LifeCycleChildComponent implements OnInit {
+export class LifeCycleChildComponent implements OnInit, OnDestroy, OnChanges, AfterContentInit, AfterViewInit {
 
   @Input() name: string;
   @Input() age: number;
   @Input() favoriteFood: string;
-  constructor() { }
 
+  public events: LifeCycleEvent[] = [];
+  nextEventId: number = 0;
+  color: string;
+  colors: string[] = ['accent', 'warn', 'primary'];
+
+  constructor() {
+    console.log(this.name + ' constructor');
+    this.newEvent('constructor');
+   }
+
+  // (Inicializador) Chamado depois do onChanges, ele mostra que foi iniciado
+  // Amplamente utilizado para fazer inicializações no template
   ngOnInit() {
+    console.log(this.name + ' onInit');
+    this.newEvent('onInit');
   }
 
+  ngOnDestroy() {
+    console.log(this.name + ' onDestroy');
+    this.newEvent('onDestroy');
+  }
+
+  // (Modificação) A cada mudança em cada componente vai ser chamada a função
+  ngOnChanges(changes: SimpleChanges) {
+    console.log(this.name + ' onChanges');
+    console.log(changes);
+    this.newEvent('onChanges');
+  //   for (let change in changes) {
+  //     console.log(change);
+  //     console.log(changes[change]);
+  // }
+  // if (changes.name) {
+  //   console.log('Previous name: ' + changes.name.previousValue);
+  //   console.log('New name: ' + changes.name.currentValue);
+  // }
+  }
+
+  ngAfterContentInit() {
+    console.log(this.name + ' afterInit');
+    this.newEvent('afterInit');
+  }
+
+  ngAfterViewInit() {
+    console.log(this.name + ' afterViewInit');
+    this.newEvent('afterViewInit');
+  }
+
+  newEvent(nameEvent: string) {
+    let i = this.nextEventId++;
+    this.events.push(
+      {
+        id: i,
+        color: this.colors[i % this.colors.length],
+        name: nameEvent
+      });
+    setTimeout(() => {
+        let idx = this.events.findIndex((e) => e.id === i);
+        if (idx >= 0) {
+          this.events.splice(idx, 1);
+        }
+    }, 3000 + this.events.length * 2000);
+  }
 }
--- lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
+++ lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
@@ -7,26 +7,49 @@ import { Client } from '../client.interface';
   styleUrls: ['./main-lifecycle.component.css']
 })
 export class MainLifecycleComponent implements OnInit {
-  private foods: string[] = ['Rice', 'Beans', 'Pizza'];
+
+  private foods: string[] = ['Rice', 'Beans', 'Pizza', 'Bread'];
   private clients: Array<Client> = [];
   private name: string;
   private age: number;
   private food: string;
-  private editClient: Client = null;
+  private editClient: number = -1;
   constructor() {}
 
   ngOnInit() {}
 
   save() {
-    if (this.editClient == null) {
+    if (this.editClient === -1) {
       this.clients.push({
         name: this.name,
         age: this.age,
         food: this.food
       });
+    } else {
+      this.clients[this.editClient].age = this.age;
+      this.clients[this.editClient].name = this.name;
+      this.clients[this.editClient].food = this.food;
+      this.editClient = -1;
     }
     this.age = null;
     this.name = '';
     this.food = '';
+  }
+
+  refresh() {
+    this.age = null;
+    this.name = '';
+    this.food = '';
+  }
+
+  edit(i: number) {
+    this.age = this.clients[i].age;
+    this.name = this.clients[i].name;
+    this.food = this.clients[i].food;
+    this.editClient = i;
+  }
+
+  delete(i: number) {
+    this.clients.splice(i, 1);
   }
 }
